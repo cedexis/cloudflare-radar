e8db44d38f3c57987236be9abdb3539a2eabf1fd
Alex Wenckus <alex.wenckus@cedexis.com>
2012-08-23 14:33:52 -0700

    Getting rid of wrapper

--- public/javascripts/cedexis-radar.js
+++ public/javascripts/cedexis-radar.js
@@ -1,11 +1,9 @@
 CloudFlare.define("cedexis-radar", ["cedexis-radar/config"],
     function(config) {
-        (function (d, c) {
-            var a = d.createElement('script');
-            a.type = 'text/javascript';
-            a.async = 'async';
-            a.src = '//' + "portal.dev.cedexis.com/api/partners/cloudflare/radar?domain_id=" + c.domain_id;
-            d.body.appendChild(a);
-        })(document, {domain_id:config.domain_id});
+        var a = document.createElement('script');
+        a.type = 'text/javascript';
+        a.async = 'async';
+        a.src = '//' + "portal.dev.cedexis.com/api/partners/cloudflare/radar?domain_id=" + config.domain_id;
+        document.body.appendChild(a);
     }
 );
