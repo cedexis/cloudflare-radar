d89977544d2ca27a81c3d84962253d63910249a0
Greg Grillo <greg.grillo@cedexis.com>
2013-11-26 15:52:29 -0800

    change to production endpoint

--- public/javascripts/cedexis-radar.js
+++ public/javascripts/cedexis-radar.js
@@ -3,7 +3,7 @@ CloudFlare.define("cedexis-radar", ["cedexis-radar/config"],
         var a = document.createElement('script');
         a.type = 'text/javascript';
         a.async = 'async';
-        a.src = '//' + "portal.dev.cedexis.com/api/partners/cloudflare/radar?domain_id=" + config.domain_id;
+        a.src = '//' + "portal.cedexis.com/api/partners/cloudflare.json/radar?domain_id=" + config.domain_id;
         document.body.appendChild(a);
     }
 );
